8f8b5ac2855b2245f70261c160d3baa21dbdbdfc
drtoxic69 <shivakumarjagadish12@gmail.com>
2020-12-07 17:26:33 +0530

    modified test.py

--- The code(object)/test.py
+++ The code(object)/test.py
@@ -1,20 +1,8 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
+import pyttsx3
 
+engine = pyttsx3.init()
 
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle('Dr. Toxic')
-
-    label = QtWidgets.QLabel(win)
-    label.setText('Hello World! ')
-    label.move(50, 50)
-    
-    win.show()
-    sys.exit(app.exec_())
-
-window()
-
+# rate = engine.getProperty('rate')   
+# engine.setProperty('rate', 250)
+engine.say('i was raised by the wolves, ate till they full')
+engine.runAndWait()
