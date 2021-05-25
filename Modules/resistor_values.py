5f27234d7622a68746f26c8fa64d53f92ad9bb8c
drtoxic69 <shivakumarjagadish12@gmail.com>
2021-05-25 13:45:17 +0530

    created a new circuitcal, trying to make the code cleaner

--- Modules/resistor_values.py
+++ Modules/resistor_values.py
@@ -1,2 +1,10 @@
-def resistor_values():
-    pass
+from numpy import reciprocal
+
+def resistor_values(**resistorValues):
+    seriesValues = sum(resistorValues['seriesValues'])
+    parallelValues = 0
+    for req in resistorValues['parallelValues']:
+        parallelValues += 1/req 
+    return seriesValues, 1/parallelValues
+    
+print(resistor_values(seriesValues=[10,20,30,40], parallelValues=[10,20,30,40]))
--- Modules/series.py
+++
@@ -1,9 +0,0 @@
-def series_circuit(*resistorValues):
-    print(sum(resistorValues))
-
-series_circuit(10, 20, 30)
-# 
-# le
-# did that work? hg
-# one sec
-# yes ig
